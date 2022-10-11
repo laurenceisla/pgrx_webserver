e3d55014c960c414e0d71c6337f74e9a0c528aa1
Laurence Isla <lau.isla.c@gmail.com>
2022-10-10 19:39:21 -0500

    Add a simple TcpListener + httparse single threaded web server

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,67 @@
+mod web_server;
+
+use std::net::TcpListener;
 use pgx::*;
+use pgx::datum::{FromDatum, IntoDatum};
+use pgx::log;
+use pgx::bgworkers::{BackgroundWorker, BackgroundWorkerBuilder, SignalWakeFlags};
+
+// Tutorial: https://github.com/tcdi/pgx/blob/master/pgx-examples/bgworker/src/lib.rs
+#[pg_guard]
+pub extern "C" fn _PG_init() {
+    BackgroundWorkerBuilder::new("Background Web Service")
+        .set_function("run_service")
+        .set_library("pgx_demo")
+        .set_argument(42i32.into_datum())
+        .enable_spi_access()
+        .load();
+}
 
 pg_module_magic!();
 
 #[pg_extern]
 fn hello_pgx_demo() -> &'static str {
     "Hello, pgx_demo"
+}
+
+#[pg_guard]
+#[no_mangle]
+pub extern "C" fn run_service(arg: pg_sys::Datum) {
+    let arg = unsafe { i32::from_datum(arg, false, pg_sys::INT4OID) };
+
+    // From the tutorial:
+    // these are the signals we want to receive.  If we don't attach the SIGTERM handler, then
+    // we'll never be able to exit via an external notification
+    BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGHUP | SignalWakeFlags::SIGTERM);
+
+    // From the tutorial:
+    // we want to be able to use SPI against the specified database (postgres), as the superuser which
+    // did the initdb. You can specify a specific user with Some("my_user")
+    BackgroundWorker::connect_worker_to_spi(Some("postgres"), None);
+
+    log!(
+        "Hello from inside the {} BGWorker!  Argument value={}",
+        BackgroundWorker::get_name(),
+        arg.unwrap()
+    );
+
+    let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
+
+    for stream in listener.incoming() {
+        // Currently this is blocking, a SIGTERM will not stop the worker
+        // until another request is made.
+        // Possible solutions:
+        // https://www.zupzup.org/epoll-with-rust/
+        // https://doc.rust-lang.org/std/net/struct.TcpListener.html#method.set_nonblocking
+        // Use tokio
+        if BackgroundWorker::sigterm_received() {
+            break;
+        }
+
+        let stream = stream.unwrap();
+
+        web_server::handle_connection(stream).expect("TODO: panic message");
+    }
 }
 
 #[cfg(any(test, feature = "pg_test"))]
---
+++ src/web_server.rs
@@ -0,0 +1,65 @@
+use std::{
+    // fs,
+    io::{prelude::*},
+    net::{TcpStream},
+};
+use std::io::{BufReader, BufRead};
+use httparse::Status::{Complete, Partial};
+use pgx::log;
+
+pub fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+
+    const NUM_OF_HEADERS: usize = 16;
+
+    let mut headers = [httparse::EMPTY_HEADER; NUM_OF_HEADERS];
+    let mut request = httparse::Request::new(&mut headers);
+
+    let mut buf_reader = BufReader::new(&mut stream);
+
+    let received = buf_reader.fill_buf().unwrap();
+
+    let result = request.parse(received);
+
+    match result {
+        Ok(Complete(_n)) => {
+            log!("Complete request.")
+        },
+        Ok(Partial) => {
+            log!("Partial request.");
+        },
+        Err(_e) => {
+            log!("Error parsing the request.");
+            // TODO: Add more info about the error
+        }
+    }
+
+    let method = request.method.unwrap();
+    let path = request.path.unwrap();
+
+    // Note: it DOES work with static paths to the webpages.
+    // TODO: find a way to get the dir path with pgx (it returns the path of the pg storage)
+    let (status, filename) =
+        if method == "GET" && path == "/" {
+            ("HTTP/1.1 200 OK", "<h1>Hello world!</h1>")
+            // ("HTTP/1.1 200 OK", "/path/to/pgx_demo/web/html/index.html")
+        }
+        else {
+            ("HTTP/1.1 400 NOT FOUND", "<h1>404 Not Found</h1>")
+            // ("HTTP/1.1 400 OK", "/path/to/pgx_demo/web/html/404.html")
+        };
+
+    let length = received.len();
+    buf_reader.consume(length);
+
+    // let contents = fs::read_to_string(filename).unwrap();
+    let contents = filename;
+    let length = contents.len();
+
+    let response = format!(
+        "{status}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap();
+
+    Ok(())
+}
